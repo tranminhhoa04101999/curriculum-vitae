4b3d468a5a6d0aee4534757e6fa3d08e4efed112
hoa <tranminhhoa04101999@gmail.com>
2022-03-15 00:44:54 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename="/cv">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
