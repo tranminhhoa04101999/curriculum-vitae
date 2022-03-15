26d4eb9b55bab049f6ff75b455c8df577696792c
hoa <tranminhhoa04101999@gmail.com>
2022-03-15 11:13:11 +0700

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/about" element={<About />}></Route>
+      <Route path="*" element={<Home to="/" replace />} />
     </Routes>
   );
 }
